9287c80fcee28edf08dbc0a853e9f2391147ae6a
Beau Dworak <beaud@thinairoutdoors.com>
2022-01-08 14:45:20 -0700

    got accurate roll and pitch values

--- app.py
+++ app.py
@@ -2,18 +2,10 @@ from flask import Flask, render_template
 import subprocess, sys
 import os
 import time
-sys.path.insert(1, '/home/pi/Mpu-Webui/mpu')
-import MPU6050
-import time
-mpu = MPU6050.MPU6050()
-mpu.dmp_initialize()
-y = 0
-p = 0
-r = 0
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html', r=r, p=p, y=y)
+    return render_template('index.html')
 @app.after_request
 def add_header(response):
     response.cache_control.max_age = 300
--- rpy.py
+++ rpy.py
@@ -1,16 +1,63 @@
+import smbus
+import math
 import time
-import sys
-sys.path.insert(1, '/home/pi/Mpu-Webui/mpu')
-import MPU6050
-gyro = [0]*3
-mpu = MPU6050.MPU6050()
-mpu.dmp_initialize()
 
+# Register
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+ 
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+ 
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+ 
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+ 
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+ 
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+ 
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+
+bus = smbus.SMBus(1) 
+address = 0x68
+ 
+bus.write_byte_data(address, power_mgmt_1, 0)
+ 
+ 
+gyroscope_xout = read_word_2c(0x43)
+gyroscope_yout = read_word_2c(0x45)
+gyroscope_zout = read_word_2c(0x47)
+ 
+#print "gyroscope_xout: ", ("%5d" % gyroscope_xout), " scaled: ", (gyroscope_xout / 131)
+#print "gyroscope_yout: ", ("%5d" % gyroscope_yout), " scaled: ", (gyroscope_yout / 131)
+#print "gyroscope_zout: ", ("%5d" % gyroscope_zout), " scaled: ", (gyroscope_zout / 131)
 while True:
-    gyro = mpu.get_rotation()
-    gyro = str(gyro)
-    f = open("static/rpy.txt", "w")
-    f.write(gyro)
+    acceleration_xout = read_word_2c(0x3b)
+    acceleration_yout = read_word_2c(0x3d)
+    acceleration_zout = read_word_2c(0x3f)
+    acceleration_xout_scaled = acceleration_xout / 16384.0
+    acceleration_yout_scaled = acceleration_yout / 16384.0
+    acceleration_zout_scaled = acceleration_zout / 16384.0
+    xrot = get_x_rotation(acceleration_xout_scaled, acceleration_yout_scaled, acceleration_zout_scaled)
+    yrot = get_y_rotation(acceleration_xout_scaled, acceleration_yout_scaled, acceleration_zout_scaled)
+    allrot = ("["+str(xrot)+", "+str(yrot)+", 180]")
+    f = open("/home/pi/Mpu-Webui/static/rpy.txt", "w")
+    f.write(allrot)
     f.close()
-    time.sleep(0.1)
-    print(gyro)
+#    print(allrot)
+    time.sleep(0.01)
