341cd05bc856c1dd8de8994ee5f84415ca9b3889
Beau Dworak <beaud@thinairoutdoors.com>
2022-01-21 15:51:11 -0700

    Updated code for motor

--- app.py
+++ app.py
@@ -3,24 +3,24 @@ import subprocess, sys
 import os
 import time
 import RPi.GPIO as GPIO
-ledPin = 13
+motorPin = 13
 GPIO.setmode(GPIO.BOARD) # use PHYSICAL GPIO Numbering
-GPIO.setup(ledPin, GPIO.OUT) # set the ledPin to OUTPUT mode
-GPIO.output(ledPin, GPIO.LOW) # make ledPin output LOW level 
+GPIO.setup(motorPin, GPIO.OUT) # set the motorPin to OUTPUT mode
+GPIO.output(motorPin, GPIO.LOW) # make motorPin output LOW level 
 GPIO.setwarnings(False)
 
-def switch_light():
-    if (GPIO.input(ledPin) == 0):
-        GPIO.output(ledPin, GPIO.HIGH)
+def switch_motor():
+    if (GPIO.input(motorPin) == 0):
+        GPIO.output(motorPin, GPIO.HIGH)
     else:
-        GPIO.output(ledPin, GPIO.LOW)
+        GPIO.output(motorPin, GPIO.LOW)
 
 app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if (request.method == 'POST'):
-        print("posted")
-        switch_light()
+        #print("posted")
+        switch_motor()
     return render_template('index.html')
 
 @app.after_request
